4024aeb3f51ab96cb6a4c08868602c808f77b66a
Shingie Taira <tairashingie@gmail.com>
2024-07-12 11:17:42 +0900

    activated movie seeds 10k

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,48 +11,48 @@ require 'open-uri'
 require 'net/http'
 require 'json'
 
-# Clear existing Movie records before seeding
-# puts "Destroying Movie Database..."
-# # Movie.destroy_all
-# puts "DESTROYED"
+Clear existing Movie records before seeding
+puts "Destroying Movie Database..."
+Movie.destroy_all
+puts "DESTROYED"
 
-# base_url = "https://api.themoviedb.org/3/movie/popular"
-# api_key = ENV['TMDB_API_KEY']
+base_url = "https://api.themoviedb.org/3/movie/popular"
+api_key = ENV['TMDB_API_KEY']
 
-# page = 1
-# max_pages = 500  # Limit to 500 pages
-# total_movies = 0  # Initialize total movies counter
+page = 1
+max_pages = 500  # Limit to 500 pages
+total_movies = 0  # Initialize total movies counter
 
-# while page <= max_pages
-#   uri = URI("#{base_url}?api_key=#{api_key}&page=#{page}")
-#   response = Net::HTTP.get(uri)
-#   result = JSON.parse(response)
+while page <= max_pages
+  uri = URI("#{base_url}?api_key=#{api_key}&page=#{page}")
+  response = Net::HTTP.get(uri)
+  result = JSON.parse(response)
 
-#   result['results'].each do |movie_hash|
-#     next if Movie.exists?(title: movie_hash['title'])
-#     next if Movie.exists?(overview: movie_hash['overview'])
-#     next if movie_hash['overview'].blank?
+  result['results'].each do |movie_hash|
+    next if Movie.exists?(title: movie_hash['title'])
+    next if Movie.exists?(overview: movie_hash['overview'])
+    next if movie_hash['overview'].blank?
 
-#     # Create Movie records from the API response
-#     Movie.create!(
-#       title: movie_hash['title'],
-#       overview: movie_hash['overview'],
-#       poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
-#       rating: movie_hash['vote_average']
-#       # Add more fields as needed
-#     )
-#     total_movies += 1
-#     # Add message after every 100 movies seeded
-#     if total_movies % 100 == 0
-#       puts "Seeded #{total_movies} movies..."
-#     end
-#   end
+    # Create Movie records from the API response
+    Movie.create!(
+      title: movie_hash['title'],
+      overview: movie_hash['overview'],
+      poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
+      rating: movie_hash['vote_average']
+      # Add more fields as needed
+    )
+    total_movies += 1
+    # Add message after every 100 movies seeded
+    if total_movies % 100 == 0
+      puts "Seeded #{total_movies} movies..."
+    end
+  end
 
-#   page += 1
-# end
+  page += 1
+end
 
-# puts "Finished fetching movies up to page #{max_pages}"
-# puts "Total movies fetched: #{total_movies}"
+puts "Finished fetching movies up to page #{max_pages}"
+puts "Total movies fetched: #{total_movies}"
 
 
 puts "Entering List and Bookmarks Seeds"
