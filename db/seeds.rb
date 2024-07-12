9ff2d4ca10fad6d271719b981ca14b34f3e5095b
Shingie Taira <tairashingie@gmail.com>
2024-07-12 10:54:06 +0900

    list and bookmark seed first run

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,64 +7,67 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require 'uri'
+require 'open-uri'
 require 'net/http'
 require 'json'
 
 # Clear existing Movie records before seeding
-puts "Destroying Movie Database"
-# Movie.destroy_all
-puts "DESTROYED"
+# puts "Destroying Movie Database..."
+# # Movie.destroy_all
+# puts "DESTROYED"
 
-base_url = "https://api.themoviedb.org/3/movie/popular"
-api_key = ENV['TMDB_API_KEY']
+# base_url = "https://api.themoviedb.org/3/movie/popular"
+# api_key = ENV['TMDB_API_KEY']
 
-page = 1
-max_pages = 500  # Limit to 500 pages
-total_movies = 0  # Initialize total movies counter
+# page = 1
+# max_pages = 500  # Limit to 500 pages
+# total_movies = 0  # Initialize total movies counter
 
-while page <= max_pages
-  uri = URI("#{base_url}?api_key=#{api_key}&page=#{page}")
-  response = Net::HTTP.get(uri)
-  result = JSON.parse(response)
+# while page <= max_pages
+#   uri = URI("#{base_url}?api_key=#{api_key}&page=#{page}")
+#   response = Net::HTTP.get(uri)
+#   result = JSON.parse(response)
 
-  result['results'].each do |movie_hash|
-    next if Movie.exists?(title: movie_hash['title'])
-    next if Movie.exists?(overview: movie_hash['overview'])
-    next if movie_hash['overview'].blank?
+#   result['results'].each do |movie_hash|
+#     next if Movie.exists?(title: movie_hash['title'])
+#     next if Movie.exists?(overview: movie_hash['overview'])
+#     next if movie_hash['overview'].blank?
 
-    # Create Movie records from the API response
-    Movie.create!(
-      title: movie_hash['title'],
-      overview: movie_hash['overview'],
-      poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
-      rating: movie_hash['vote_average']
-      # Add more fields as needed
-    )
-    total_movies += 1
-    # Add message after every 100 movies seeded
-    if total_movies % 100 == 0
-      puts "Seeded #{total_movies} movies..."
-    end
-  end
+#     # Create Movie records from the API response
+#     Movie.create!(
+#       title: movie_hash['title'],
+#       overview: movie_hash['overview'],
+#       poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
+#       rating: movie_hash['vote_average']
+#       # Add more fields as needed
+#     )
+#     total_movies += 1
+#     # Add message after every 100 movies seeded
+#     if total_movies % 100 == 0
+#       puts "Seeded #{total_movies} movies..."
+#     end
+#   end
 
-  page += 1
-end
+#   page += 1
+# end
 
-puts "Finished fetching movies up to page #{max_pages}"
-puts "Total movies fetched: #{total_movies}"
+# puts "Finished fetching movies up to page #{max_pages}"
+# puts "Total movies fetched: #{total_movies}"
 
 
-# seeds.rb
 puts "Entering List and Bookmarks Seeds"
-# Array of list names
-list_names = [
-  "horror",
-  "Comedy",
-  "Action",
-  "Romance",
-  "Autobio",
-  "Sexuality"
+puts "Destroying Lists"
+List.destroy_all
+puts "Destroyed Lists"
+
+# Array of list names with associated image URLs
+list_data = [
+  { name: "horror", image_url: "https://res.cloudinary.com/ddzvfukq6/image/upload/v1720716160/production/751fg8r8w1q09gs8ku731wrxa8nk.jpg" },
+  { name: "Comedy", image_url: "https://res.cloudinary.com/ddzvfukq6/image/upload/v1720716238/production/h9ft2s9oz748o92rxhwt0ndcmmz9.jpg" },
+  { name: "Action", image_url: "https://res.cloudinary.com/ddzvfukq6/image/upload/v1720716314/production/kpqlolxggobm4kgruoc7b9bb14qz.jpg" },
+  { name: "Romance", image_url: "https://res.cloudinary.com/ddzvfukq6/image/upload/v1720716348/production/cyfo0f7k7ckggzlmx401pdtey2g0.webp" },
+  { name: "Autobio", image_url: "https://res.cloudinary.com/ddzvfukq6/image/upload/v1720716427/production/zlvf63257n2ahme9fsoiz4htmrho.jpg" },
+  { name: "Sexuality", image_url: "https://res.cloudinary.com/ddzvfukq6/image/upload/v1720716542/production/b15ebe4dx69wgvku7m7orvf9htrd.png" }
 ]
 
 # Array of bookmark attributes
@@ -79,12 +82,26 @@ bookmarks_attributes = [
 
 puts "seeding Lists..."
 # Seed Lists
-list_names.each do |name|
-  List.find_or_create_by(name: name)
-  puts "seeding #{name}"
+list_data.each do |data|
+  list = List.find_or_create_by(name: data[:name])
+  puts "seeding #{data[:name]}"
+  
+  # Attach image from Cloudinary
+  if list.picture.attached?
+    list.picture.purge
+  end
+  list.picture.attach(
+    io: URI.open(data[:image_url]),
+    filename: "#{data[:name]}.jpg",
+    content_type: 'image/jpeg'
+  )
+  puts "Attached image of #{data[:name]}
 end
 puts "Done seeding Lists"
 
+puts "Destroying Bookmarks"
+Bookmark.destroy_all
+puts "Destroyed Bookmarks"
 puts "seeding Bookmarks..."
 # Seed Bookmarks
 bookmarks_attributes.each do |attributes|
