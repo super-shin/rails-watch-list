522d2552879075d71bac0295901bf691e4abc97a
Shingie Taira <tairashingie@gmail.com>
2024-07-22 23:28:51 +0900

    added slim select library for advanced searching

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true # @5.3.3
 pin "@popperjs/core", to: "popper.js", preload: true # @2.11.8
+pin "slim-select" # @2.8.2
